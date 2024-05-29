d4a37a81497ae3df5b1d6b655bf32491aa7ee8bf
merulox <codeberg.vixen352@passmail.net>
2024-05-29 19:04:41 -0400

    things

--- comment-widget-dark/comment-widget.js
+++ comment-widget-dark/comment-widget.js
@@ -34,7 +34,7 @@ const s_dstStart = ['March', 'Sunday', 2, 2]; // Example shown is the second Sun
 const s_dstEnd = ['November', 'Sunday', 1, 2]; // Example shown is the first Sunday of November at 2:00 am
 
 // Misc - Other random settings
-const s_commentsPerPage = 15; // The max amount of comments that can be displayed on one page, any number >= 1 (Replies not counted)
+const s_commentsPerPage = 20; // The max amount of comments that can be displayed on one page, any number >= 1 (Replies not counted)
 const s_maxLength = 1000; // The max character length of a comment
 const s_maxLengthName = 30; // The max character length of a name
 const s_commentsOpen = true; // Change to false if you'd like to close your comment section site-wide (Turn it off on Google Forms too!)
